f0277f2c46c48768451efb2b0b8e353950ce0c5a
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-20 17:59:08 -0300

    feat: add TicketRepository and DTO

---
+++ server/src/DTOs/ticketDTO.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const createTicketDTO = z.object({
+  userId: z.string().uuid({ message: "O userId deve ser um UUID válido." }),
+  eventId: z.string().uuid({ message: "O eventId deve ser um UUID válido." }),
+});
+
+export type CreateTicketDTO = z.infer<typeof createTicketDTO>
---
+++ server/src/repositories/ticketRepository.ts
@@ -0,0 +1,19 @@
+import type { CreateTicketDTO } from "../DTOs/ticketDTO.js";
+import { prisma } from "../database/index.js";
+
+class TicketRepository {
+  async create(data: CreateTicketDTO) {
+    const ticket = await prisma.ticket.create({ data });
+    return ticket;
+  }
+
+  async findById(id: string) {
+    const ticket = await prisma.ticket.findUnique({ where: { id } });
+    return ticket;
+  }
+
+  async findByUserId(id: string) {
+    const ticket = await prisma.ticket.findUnique({ where: { id } });
+    return ticket;
+  }
+}
