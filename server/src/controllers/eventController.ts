c4f74c91f5807791d9d24461987159b73b9e36be
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-21 17:22:20 -0300

    feat: implement EventController for event management with create, update, delete, findById, and findMany methods

---
+++ server/src/controllers/eventController.ts
@@ -0,0 +1,35 @@
+import type { Request, Response } from "express";
+import eventService from "../services/eventService.js";
+import type { CreateEventDTO, UpdateEventDTO } from "../DTOs/eventDTO.js";
+
+class EventController {
+  async create(req: Request, res: Response) {
+    const data = req.body as CreateEventDTO;
+    const event = await eventService.create(data);
+    return res.status(201).json(event);
+  }
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const data = req.body as UpdateEventDTO;
+    const event = await eventService.update(id as string, data);
+    return res.status(200).json(event);
+  }
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    const event = await eventService.delete(id as string);
+    return res.status(200).json(event)
+  }
+  async findById(req: Request, res:Response) {
+    const { id } = req.params;
+    const event = await eventService.findById(id as string);
+    return res.status(200).json(event);
+  }
+
+  async findMany(req: Request, res: Response) {
+    const { onlyUpcoming } = req.query;
+    const events = await eventService.findMany(onlyUpcoming === "true");
+    return res.status(200).json(events);
+  }
+}
+
+export default new EventController();
