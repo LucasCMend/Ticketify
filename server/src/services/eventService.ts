3757a66d93bcb1d3cc851f53a39d0a57267722e6
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-21 16:45:52 -0300

    feat: implement EventService for event management with create, update, delete, and find methods

---
+++ server/src/services/eventService.ts
@@ -0,0 +1,98 @@
+import type { Prisma } from "@prisma/client";
+import { AppError } from "../errors/AppError.js";
+import eventRepository from "../repositories/eventRepository.js";
+import { only } from "node:test";
+
+class EventService {
+  async create(data: Prisma.EventCreateInput) {
+    const eventDate = new Date(data.date);
+
+    if (eventDate < new Date()) {
+      throw new AppError(
+        "A data do evento não deve ser anterior à data atual.",
+        400,
+      );
+    }
+
+    const eventData = { ...data, availableTickets: data.totalTickets };
+    return await eventRepository.create(eventData);
+  }
+
+  async update(id: string, data: Prisma.EventUpdateInput) {
+    const event = await this.findById(id);
+
+    if (!event) {
+      throw new AppError("Evento não encontrado.", 404);
+    }
+
+    if (data.date) {
+      const eventDate = new Date(data.date as string | Date);
+      if (eventDate < new Date()) {
+        throw new AppError(
+          "A data do evento não deve ser anterior à data atual.",
+          400,
+        );
+      }
+    }
+    if (data.totalTickets !== undefined) {
+      const newTotalTickets = Number(data.totalTickets);
+      const soldTickets = event.totalTickets - event.availableTickets;
+
+      if (newTotalTickets < soldTickets) {
+        throw new AppError(
+          `Não é possível reduzir a capacidade para ${newTotalTickets}. Já existem ${soldTickets} ingressos vendidos.`,
+          400,
+        );
+      }
+      if (newTotalTickets <= 0) {
+        throw new AppError(
+          `Não é possível reduzir a capacidade para um número menor que 0.`,
+          400,
+        );
+      }
+      const newAvailableTickets =
+        event.availableTickets + (newTotalTickets - event.totalTickets);
+      data.availableTickets = newAvailableTickets;
+      data.totalTickets = newTotalTickets;
+    }
+    return await eventRepository.update(id, data);
+  }
+
+  async delete(id: string) {
+    const event = await eventRepository.findById(id);
+
+    if (!event) {
+      throw new AppError("Evento não encontrado.", 404);
+    }
+    const soldTickets = event.totalTickets - event.availableTickets;
+
+    if (soldTickets > 0) {
+        throw new AppError(
+          `Não é possível deletar o evento. Já existem ${soldTickets} ingressos vendidos.`,
+          400,
+        );
+    }
+    return await eventRepository.delete(id);
+  }
+
+  async findById(id: string) {
+    const event = await eventRepository.findById(id);
+
+    if (!event) {
+      throw new AppError("Evento não encontrado.", 404);
+    }
+    return event;
+  }
+
+  async findMany(onlyUpcoming: boolean) {
+    const events = await eventRepository.findMany()
+    const today = new Date()
+
+    if (onlyUpcoming) {
+        return events.filter(event => new Date(event.date) > today)
+    }
+    return events.filter(event => new Date(event.date) < today)
+  }
+}
+
+export default new EventService();
