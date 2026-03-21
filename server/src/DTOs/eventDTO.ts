59f5b9fa11800f24c8ae5fc6dd8b68c416ae51e8
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-21 13:24:39 -0300

    feat: enhance event DTOs with optional fields for update

--- server/src/DTOs/eventDTO.ts
+++ server/src/DTOs/eventDTO.ts
@@ -8,12 +8,28 @@ const createEventDTO = z.object({
   date: z.string().refine((date) => !isNaN(Date.parse(date)), {
     message: "A data deve ser uma string de data válida.",
   }),
+  totalTickets: z.number().int().positive({
+    message: "O total de ingressos deve ser um número inteiro positivo.",
+  }),
+});
+
+const updateEventDTO = z.object({
+  name: z.string().min(1).optional(),
+  description: z.string().min(1).optional(),
+  date: z
+    .string()
+    .refine((date) => !isNaN(Date.parse(date)), {
+      message: "A data deve ser uma string de data válida.",
+    })
+    .optional(),
   totalTickets: z
     .number()
     .int()
     .positive({
       message: "O total de ingressos deve ser um número inteiro positivo.",
-    }),
+    })
+    .optional(),
 });
 
 export type CreateEventDTO = z.infer<typeof createEventDTO>;
+export type UpdateEventDTO = z.infer<typeof updateEventDTO>;
