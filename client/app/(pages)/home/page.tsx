eebc62f94b2c73500bb28c4272bfc3fedfae7094
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-27 20:42:27 -0300

    feat: add Home page and NavBar component for main layout

---
+++ client/app/(pages)/home/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/app/components/navBar"
+
+export default function Home() {
+    return(
+        <div className="w-full min-h-screen bg-slate-100">
+            <div className="">
+                <NavBar></NavBar>
+            </div>
+        </div>
+    )
+}
