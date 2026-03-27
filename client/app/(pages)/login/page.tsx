999e28cf21d5697db83396d8f31d4c345a565e9e
Lucas Mendonça <lucascmendoncaa@gmail.com>
2026-03-27 12:13:42 -0300

    feat: create LoginCard component for user authentication

--- client/app/(pages)/login/page.tsx
+++ client/app/(pages)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react";
-import LoginCard from "@/app/components/LoginCard/loginCard";
+import LoginCard from "@/app/components/LoginCard";
 
 export default function Login() {
 
